2ac9e4f053d60d968bd95f78952dd3435733127a
adrlvma <adrlvma741@gmail.com>
2021-03-04 16:41:56 -0300

    Adicionando cabeçalho e importações.

---
+++ cliente_py.py
@@ -0,0 +1,18 @@
+"""
+mar 4 2021 -  4:20 PM
+
+Desenvolvedores:
+    @AdrlVMA & @MayconCarvalho
+
+-----------------------------------------
+Classe Cliente - Comunicação RCP
+-----------------------------------------
+
+
+Fontes:
+
+
+"""
+
+from datetime import datetime
+from threading import Thread
